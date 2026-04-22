132cf588262e3fe785939ca1f73e292d61820c73
Greta-Alfredsdottir <eldvakinn@hotmail.com>
2026-04-22 09:29:46 +0200

    new

--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -1,7 +1,19 @@
-
+import { useEffect, useState } from "react"
 
 export function History() {
+    const [history, setHistory] = useState();
+    const url = "https://www.elprisenligenu.dk/api/v1/prices/[ÅR]/[MÅNED]-[DAG]_[PRISKLASSE].json "
 
+    useEffect(()=> {
+        async function history(){
+            const res = await fetch(url)
+            if(!res.ok){
+                    throw new Error('Failed to fetch')}
+            const data = await res.json()
+            setData(data)
+        }
+        history()
+    },[])
 
     return(
         <>
--- src/pages/oversigt.jsx
+++ src/pages/oversigt.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react"
+
 export function Oversigt() {
+    const [oversigt, setOversigt] = useState()
+    const url = ("https://www.elprisenligenu.dk/api/v1/prices/[ÅR]/[MÅNED]-[DAG]_[PRISKLASSE].json ")
+        useEffect(() => {
+            async function oversigt() {
+                const res =await fetch(url)
+                const data = await res.json()
+                setOversigt(data)
+                
+            }
+            oversigt
+        }, [])
+    
 
     return(
         <h1>Oversigt</h1>
